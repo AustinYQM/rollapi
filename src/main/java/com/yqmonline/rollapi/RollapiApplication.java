11894b508e1bd006667926db6bf2d67858b928e8
AustinYQM <a.r.mcgowan@gmail.com>
2020-06-10 19:40:31 -0500

    Initial commit

---
+++ src/main/java/com/yqmonline/rollapi/RollapiApplication.java
@@ -0,0 +1,13 @@
+package com.yqmonline.rollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollapiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RollapiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yqmonline/rollapi/RollapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqmonline.rollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollapiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
